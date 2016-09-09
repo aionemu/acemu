2e9aa9422fd762c3db32153535891ab235b3fb43
ixren <xren@aionchs.com>
2016-09-09 09:23:38 +0800

    2

---
+++ AionWeb/files/copyrightcommon.js
@@ -0,0 +1,4 @@
+﻿var gw = '<p class="add1026"><a href="http://www.shandagames.com/" title="盛大游戏官网" target="_blank">盛大游戏官网</a>&nbsp;&nbsp;|<a href="http://hr.snda.com" title="招聘" target="_blank"><span>招聘</span></a></p>';
+
+var container = document.getElementById("containerDiv");
+container.innerHTML = gw;
